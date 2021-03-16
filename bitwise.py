8a80bda4450d4012c3af81a190cde09583ba071e
thanhffan <d3ars0me1@gmail.com>
2021-03-16 21:22:24 +0700

    Set 1 - Challenge 3

--- bitwise.py
+++ bitwise.py
@@ -1,12 +1,26 @@
 
 # Return a bit string
 def xor_two_bit_string(str1, str2):
-    if len(str1) == 0 | len(str2) == 0:
-        return ""
-    
+    if len(str1) < len(str2):
+        loop = True
+        while loop:
+            for i in str1:
+                str1 = str1 + i
+                if len(str1) == len(str2):
+                    loop = False
+                    break
+    elif len(str1) > len(str2):
+        loop = True
+        while loop:
+            for i in str2:
+                str2 = str2 + i
+                if len(str1) == len(str2):
+                    loop = False
+                    break
+
     if len(str1) != len(str2):
         return ""
-    
+
     result = ""
     for i in range(0, len(str1)):
         sumtext = str1[i] + str2[i]
--- playground.py
+++ playground.py
@@ -1,12 +1,24 @@
 import encoding
 import bitwise
  
-print("Set 1 - Challenge 1")
-print(encoding.hexstr_to_text('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'))
-print("\n")
+#print(encoding.hexstr_to_text('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'))
+#print("\n")
+#print("Set 1 - Challenge 1")
 
-print("Set 2 - Challenge 2")
-str1 = encoding.hexstr_to_bintext("1c0111001f010100061a024b53535009181c")
-str2 = encoding.hexstr_to_bintext("686974207468652062756c6c277320657965")
-binstr = bitwise.xor_two_bit_string(str1, str2)
-print(encoding.binstr_to_hexstr(binstr))
+#print("Set 1 - Challenge 2")
+#str1 = encoding.hexstr_to_bintext("1c0111001f010100061a024b53535009181c")
+#str2 = encoding.hexstr_to_bintext("686974207468652062756c6c277320657965")
+#binstr = bitwise.xor_two_bit_string(str1, str2)
+#print(encoding.binstr_to_hexstr(binstr))
+
+print("Set 1 - Challenge 3")
+binTextCipher = encoding.hexstr_to_bintext("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+for i in range(0, 255):
+    binI = '{0:08b}'.format(i)
+    xorBinStr = bitwise.xor_two_bit_string(binTextCipher, binI)
+    xorHexStr = encoding.binstr_to_hexstr(xorBinStr)
+    decodeStr = encoding.hexstr_to_text(xorHexStr)
+    # Look like 88 is the correct one xD
+    if i == 88:
+        print(decodeStr)
+
